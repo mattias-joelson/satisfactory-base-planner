3bc66ed91c73a902a62ae781550a45511c08b399
Mattias Joëlson <mattias.joelson@looklet.com>
2021-01-13 13:20:24 +0100

    Hello, world...

---
+++ src/main/java/org/joelson/mattias/satisfactory/SatisfactoryBasePlanner.java
@@ -0,0 +1,9 @@
+package org.joelson.mattias.satisfactory;
+
+public class SatisfactoryBasePlanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
